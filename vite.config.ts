20732de7242e37eacd7d3fb488c1f3409b455a11
piyush289kumar <piyushraikwar289@gmail.com>
2025-01-29 20:53:27 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/dev-vault/", // Replace with your repo name
   plugins: [react()],
   resolve: {
     alias: {
