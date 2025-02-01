c2872d0325136eea7e6805a264d4a3cdee4bd05f
piyush289kumar <piyushraikwar289@gmail.com>
2025-02-01 19:29:58 +0530

    Add Terminal and TerminalBox components with animated typing effects

--- src/components/landiingPageUI/SoftwarePortfolioSection.jsx
+++ src/components/landiingPageUI/SoftwarePortfolioSection.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
+import { TerminalBox } from "./TerminalBox";
 
 const SoftwarePortfolioData = [
   {
@@ -27,46 +28,63 @@ const SoftwarePortfolioData = [
       [255, 255, 190], // RGB format for this card
     ],
   },
-  //   {
-  //     title: "Astro",
-  //     icon: (
-  //       <svg
-  //         xmlns="http://www.w3.org/2000/svg"
-  //         width="104"
-  //         height="42"
-  //         fill="none"
-  //       >
-  //         <path
-  //           fill="white"
-  //           d="M45.8295 35.7732C43.9321 34.0388 43.3782 30.3945 44.1687 27.7542C45.5394 29.4188 47.4386 29.9462 49.4058 30.2438C52.4428 30.7031 55.4253 30.5313 58.2465 29.1433C58.5692 28.9843 58.8675 28.773 59.2201 28.559C59.4848 29.327 59.5537 30.1023 59.4613 30.8914C59.2365 32.8133 58.28 34.2978 56.7589 35.4232C56.1506 35.8733 55.5069 36.2757 54.8787 36.7001C52.9486 38.0046 52.4265 39.5342 53.1517 41.759C53.1689 41.8136 53.1843 41.8677 53.2233 42C52.2379 41.5588 51.5181 40.9166 50.9696 40.0723C50.3904 39.1814 50.1148 38.1957 50.1002 37.1291C50.093 36.6101 50.093 36.0864 50.0232 35.5747C49.8528 34.3272 49.2671 33.7687 48.1638 33.7365C47.0316 33.7035 46.1359 34.4035 45.8984 35.5059C45.8803 35.5904 45.854 35.674 45.8277 35.7723L45.8295 35.7732Z"
-  //         />
-  //       </svg>
-  //     ),
-  //     colors: [
-  //       [255, 255, 249], // RGB format for this card
-  //       [255, 255, 249], // RGB format for this card
-  //     ],
-  //   },
-  //   {
-  //     title: "Expo",
-  //     icon: (
-  //       <svg
-  //         xmlns="http://www.w3.org/2000/svg"
-  //         width="104"
-  //         height="42"
-  //         fill="none"
-  //       >
-  //         <path
-  //           fill="#D9D9DE"
-  //           d="M50.955 16.235c.327-.482.685-.544.975-.544.29 0 .775.061 1.102.544 2.578 3.543 6.835 10.6 9.974 15.806 2.047 3.395 3.62 6.003 3.943 6.335 1.21 1.245 2.872.47 3.837-.944.95-1.391 1.214-2.368 1.214-3.41 0-.71-13.766-26.322-15.153-28.454C55.514 3.518 55.08 3 52.797 3H51.09c-2.275 0-2.604.518-3.937 2.568C45.767 7.7 32 33.312 32 34.022c0 1.042.264 2.019 1.214 3.41.965 1.413 2.626 2.19 3.837.944.323-.332 1.896-2.94 3.943-6.335 3.14-5.206 7.382-12.263 9.96-15.806Z"
-  //         />
-  //       </svg>
-  //     ),
-  //     colors: [
-  //       [255, 255, 255], // RGB format for this card
-  //       [255, 255, 255], // RGB format for this card
-  //     ],
-  //   },
+  {
+    title: "Supabase",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="104"
+        height="42"
+        fill="none"
+      >
+        <path
+          fill="#3ECF8E"
+          d="M48.868 2.837c1.157-1.518 3.108-.81 3.132.837V28H36.436c-2.87 0-4.47-3.39-2.686-5.689L48.868 2.837Z"
+        />
+        <path
+          fill="url(#paint0_linear_219_13847)"
+          d="M55.132 39.163c-1.157 1.518-3.108.81-3.132-.837V14h15.564c2.87 0 4.47 3.39 2.686 5.689L55.132 39.163Z"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_219_13847"
+            x1="50"
+            x2="67.482"
+            y1="15.5"
+            y2="30.955"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#249361" />
+            <stop offset="1" stop-color="#3ECF8E" />
+          </linearGradient>
+        </defs>
+      </svg>
+    ),
+    colors: [
+      [255, 255, 249], // RGB format for this card
+      [255, 255, 249], // RGB format for this card
+    ],
+  },
+  {
+    title: "Expo",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="104"
+        height="42"
+        fill="none"
+      >
+        <path
+          fill="#D9D9DE"
+          d="M50.955 16.235c.327-.482.685-.544.975-.544.29 0 .775.061 1.102.544 2.578 3.543 6.835 10.6 9.974 15.806 2.047 3.395 3.62 6.003 3.943 6.335 1.21 1.245 2.872.47 3.837-.944.95-1.391 1.214-2.368 1.214-3.41 0-.71-13.766-26.322-15.153-28.454C55.514 3.518 55.08 3 52.797 3H51.09c-2.275 0-2.604.518-3.937 2.568C45.767 7.7 32 33.312 32 34.022c0 1.042.264 2.019 1.214 3.41.965 1.413 2.626 2.19 3.837.944.323-.332 1.896-2.94 3.943-6.335 3.14-5.206 7.382-12.263 9.96-15.806Z"
+        />
+      </svg>
+    ),
+    colors: [
+      [255, 255, 255], // RGB format for this card
+      [255, 255, 255], // RGB format for this card
+    ],
+  },
   {
     title: "Firebase",
     icon: (
@@ -200,72 +218,72 @@ const SoftwarePortfolioData = [
     ],
   },
 
-  {
-    title: "Supabase",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="104"
-        height="42"
-        fill="none"
-      >
-        <path
-          fill="#3ECF8E"
-          d="M48.868 2.837c1.157-1.518 3.108-.81 3.132.837V28H36.436c-2.87 0-4.47-3.39-2.686-5.689L48.868 2.837Z"
-        />
-        <path
-          fill="url(#paint0_linear_219_13847)"
-          d="M55.132 39.163c-1.157 1.518-3.108.81-3.132-.837V14h15.564c2.87 0 4.47 3.39 2.686 5.689L55.132 39.163Z"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear_219_13847"
-            x1="50"
-            x2="67.482"
-            y1="15.5"
-            y2="30.955"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#249361" />
-            <stop offset="1" stop-color="#3ECF8E" />
-          </linearGradient>
-        </defs>
-      </svg>
-    ),
-    colors: [
-      [255, 255, 153], // RGB format for this card
-      [255, 255, 153], // RGB format for this card
-    ],
-  },
+  // {
+  //   title: "Supabase",
+  //   icon: (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       width="104"
+  //       height="42"
+  //       fill="none"
+  //     >
+  //       <path
+  //         fill="#3ECF8E"
+  //         d="M48.868 2.837c1.157-1.518 3.108-.81 3.132.837V28H36.436c-2.87 0-4.47-3.39-2.686-5.689L48.868 2.837Z"
+  //       />
+  //       <path
+  //         fill="url(#paint0_linear_219_13847)"
+  //         d="M55.132 39.163c-1.157 1.518-3.108.81-3.132-.837V14h15.564c2.87 0 4.47 3.39 2.686 5.689L55.132 39.163Z"
+  //       />
+  //       <defs>
+  //         <linearGradient
+  //           id="paint0_linear_219_13847"
+  //           x1="50"
+  //           x2="67.482"
+  //           y1="15.5"
+  //           y2="30.955"
+  //           gradientUnits="userSpaceOnUse"
+  //         >
+  //           <stop stop-color="#249361" />
+  //           <stop offset="1" stop-color="#3ECF8E" />
+  //         </linearGradient>
+  //       </defs>
+  //     </svg>
+  //   ),
+  //   colors: [
+  //     [255, 255, 153], // RGB format for this card
+  //     [255, 255, 153], // RGB format for this card
+  //   ],
+  // },
 
-  //   {
-  //     title: "Convex",
-  //     icon: (
-  //       <svg
-  //         xmlns="http://www.w3.org/2000/svg"
-  //         width="104"
-  //         height="42"
-  //         fill="none"
-  //       >
-  //         <path
-  //           fill="#F3B01C"
-  //           d="M57.276 32.527c5.956-.646 11.57-3.743 14.663-8.913-1.464 12.787-15.79 20.87-27.485 15.907a5.887 5.887 0 0 1-2.642-2.19c-2.627-4.027-3.491-9.152-2.25-13.803 3.546 5.974 10.757 9.636 17.714 9Z"
-  //         />
-  //         <path
-  //           fill="#8D2676"
-  //           d="M39.345 19.776c-2.415 5.445-2.52 11.82.44 17.067-10.416-7.649-10.302-24.016-.127-31.588a5.951 5.951 0 0 1 3.233-1.179c4.824-.248 9.725 1.571 13.163 4.962-6.984.068-13.786 4.434-16.71 10.738Z"
-  //         />
-  //         <path
-  //           fill="#EE342F"
-  //           d="M59.421 10.714c-3.523-4.795-9.039-8.06-15.081-8.158C56.02-2.62 70.388 5.77 71.952 18.174a5.851 5.851 0 0 1-.569 3.364c-2.182 4.325-6.228 7.68-10.957 8.922a18.736 18.736 0 0 0-1.005-19.746Z"
-  //         />
-  //       </svg>
-  //     ),
-  //     colors: [
-  //       [255, 255, 245], // RGB format for this card
-  //       [255, 255, 245], // RGB format for this card
-  //     ],
-  //   },
+  // {
+  //   title: "Convex",
+  //   icon: (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       width="104"
+  //       height="42"
+  //       fill="none"
+  //     >
+  //       <path
+  //         fill="#F3B01C"
+  //         d="M57.276 32.527c5.956-.646 11.57-3.743 14.663-8.913-1.464 12.787-15.79 20.87-27.485 15.907a5.887 5.887 0 0 1-2.642-2.19c-2.627-4.027-3.491-9.152-2.25-13.803 3.546 5.974 10.757 9.636 17.714 9Z"
+  //       />
+  //       <path
+  //         fill="#8D2676"
+  //         d="M39.345 19.776c-2.415 5.445-2.52 11.82.44 17.067-10.416-7.649-10.302-24.016-.127-31.588a5.951 5.951 0 0 1 3.233-1.179c4.824-.248 9.725 1.571 13.163 4.962-6.984.068-13.786 4.434-16.71 10.738Z"
+  //       />
+  //       <path
+  //         fill="#EE342F"
+  //         d="M59.421 10.714c-3.523-4.795-9.039-8.06-15.081-8.158C56.02-2.62 70.388 5.77 71.952 18.174a5.851 5.851 0 0 1-.569 3.364c-2.182 4.325-6.228 7.68-10.957 8.922a18.736 18.736 0 0 0-1.005-19.746Z"
+  //       />
+  //     </svg>
+  //   ),
+  //   colors: [
+  //     [255, 255, 245], // RGB format for this card
+  //     [255, 255, 245], // RGB format for this card
+  //   ],
+  // },
 ];
 
 export function SoftwarePortfolioCanvasRevealEffect() {
@@ -281,8 +299,8 @@ export function SoftwarePortfolioCanvasRevealEffect() {
           Clerk keeps up with the latest trends and security best practices.
         </p>
 
-        <div className="py-20 flex flex-wrap items-center justify-center bg-white dark:bg-black w-full  px-8">
-          <div className="flex flex-wrap justify-center w-full">
+        <div className="py-20 flex items-top gap-1 justify-center bg-white dark:bg-black w-full">
+          <div className="flex flex-wrap justify-center px-8 w-[60%]">
             {SoftwarePortfolioData.map((softwareCard) => (
               <div key={softwareCard.title} className="relative">
                 <Card title={softwareCard.title} icon={softwareCard.icon}>
@@ -298,6 +316,9 @@ export function SoftwarePortfolioCanvasRevealEffect() {
               </div>
             ))}
           </div>
+          <div className="w-[30%]">
+            <TerminalBox />
+          </div>
         </div>
       </div>
     </>
@@ -310,7 +331,7 @@ const Card = ({ title, icon, children }) => {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-[15rem] mx-auto p-4 relative min-h-[15rem]"
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-[15rem] mx-auto p-4 relative min-h-[13rem]"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
@@ -336,40 +357,6 @@ const Card = ({ title, icon, children }) => {
         </h2>
       </div>
     </div>
-  );
-};
-
-const NextJsIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="104" height="42" fill="none">
-      <path
-        fill="#D9D9DE"
-        fillRule="evenodd"
-        d="M63.054 37.67A19.907 19.907 0 0 1 52 41c-11.046 0-20-8.954-20-20S40.954 1 52 1s20 8.954 20 20c0 6-2.643 11.384-6.828 15.05l-4.95-6.407V13h-2.666v13.19L47.365 13H44v15.993h2.692V16.42L63.054 37.67Z"
-        clipRule="evenodd"
-      />
-      <path
-        fill="url(#paint0_linear_485_6386)"
-        fillOpacity=".4"
-        fillRule="evenodd"
-        d="M63.054 37.67A19.907 19.907 0 0 1 52 41c-11.046 0-20-8.954-20-20S40.954 1 52 1s20 8.954 20 20c0 6-2.643 11.384-6.828 15.05l-4.95-6.407V13h-2.666v13.19L47.365 13H44v15.993h2.692V16.42L63.054 37.67Z"
-        clipRule="evenodd"
-        style={{ mixBlendMode: "overlay" }} // Corrected style prop
-      />
-      <defs>
-        <linearGradient
-          id="paint0_linear_485_6386"
-          x1="52"
-          x2="52"
-          y1="1"
-          y2="41"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#fff" />
-          <stop offset=".775" stopColor="#fff" stopOpacity="0" />
-        </linearGradient>
-      </defs>
-    </svg>
   );
 };
 
