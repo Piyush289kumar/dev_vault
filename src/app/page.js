0d6899382aafb830aae20734bfced59eb58a396f
piyush289kumar <piyushraikwar289@gmail.com>
2025-02-01 14:21:42 +0530

    Add new SVG logos and implement WobbleCard component for enhanced UI interactivity

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 "use client";
 
+import { SoftwarePortfolioCanvasRevealEffect } from "@/components/landiingPageUI/SoftwarePortfolioSection";
+import { FeatureCard } from "@/components/landiingPageUI/WobbleCard";
 import { GridBackground } from "@/components/ui/GridBackGround";
 
 export default function Home() {
   return (
     <>
       <GridBackground />
+      <FeatureCard />
+      <SoftwarePortfolioCanvasRevealEffect />
     </>
   );
 }
