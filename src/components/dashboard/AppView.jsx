cfb8535e6bc3f90a72ab66a4ec5b41683db563eb
Piyush289kumar <piyushraikwar289@gmail.com>
2025-03-26 14:54:30 +0530

    View Create

--- src/components/dashboard/AppView.jsx
+++ src/components/dashboard/AppView.jsx
@@ -1,17 +1,16 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { LaptopMinimal, Smartphone, Tablet } from "lucide-react";
 
 export default function AppView() {
   return (
-    <Card className="flex justify-center rounded-t-none items-center overflow-hidden">
-      <CardContent className="flex flex-col items-center">
+    <Card className="flex justify-center items-center overflow-hidden bg-transparent border-none shadow-none">
+      <CardContent className="flex flex-col items-center bg-transparent">
         {/* Phone Frame */}
         <div className="flex flex-col bg-black border-[6px] border-gray-800 h-full p-0 rounded-[36px] shadow-xl w-full items-center mt-4 relative">
           {/* Speaker and Front Camera */}
           <div className="bg-gray-950 h-6 rounded-full w-24 absolute top-2"></div>
 
           {/* Screen Content */}
-          <div className="flex flex-col bg-zinc-700 h-[700px] justify-center rounded-[30px] text-center w-[380px] items-center">
+          <div className="flex flex-col bg-zinc-700 min-h-[550px] max-h-[700px] justify-center rounded-[30px] text-center min-w-[280px] max-w-[380px] items-center">
             <h2 className="text-lg font-semibold">App Preview</h2>
             
             <p className="text-gray-500">Your app screen content here...</p>
